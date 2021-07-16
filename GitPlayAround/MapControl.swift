fcc6eb2a14c0be20d0936b9cf002e7e557f7eba0
Jing Li <jingli9003@gmail.com>
2021-07-16 15:54:25 +0800

    Create MapControl.swift

---
+++ GitPlayAround/MapControl.swift
@@ -0,0 +1,20 @@
+//
+//  MapControl.swift
+//  GitPlayAround
+//
+//  Created by Jing Li on 7/16/21.
+//
+
+import UIKit
+
+class MapControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
